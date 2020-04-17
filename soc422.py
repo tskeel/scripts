a508ad6de65197ead76a63924de798e75e418531
tskeel <tskeel@users.noreply.github.com>
2020-04-17 11:09:15 -0700

    Add files via upload

--- soc422.py
+++ soc422.py
@@ -17,6 +17,17 @@ payload = "WelcomeToTheSocChallenge"
 #set the Destination IP
 uri = "3.94.231.16"
 
+#Set sleep min/max
+sleep1 = 1
+sleep2 = 2
+
+#Set Web Loop Count
+max = 2
+
+#Set Port Scan range
+range1 = 50
+range2 = 53
+
 #Set Request Headers
 HEADERS = {'user-agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/81.0.40 Safari/537.36',
                           'referer': 'http://www.twitter.com'}
@@ -27,33 +38,37 @@ time = time.strftime("%d/%m/%Y %H:%M:%S")
 print("Web Loop Start: " + time)
 
 count = 1
-while count < 5:
-    t.sleep(random.randint(1,3))
+while count < max:
+    t.sleep(random.randint(sleep1,sleep2))
     links=['http://www.google.com','http://www.twitter.com', 'http://' + uri, 'http://www.espn.com', 'http://www.reddit.com',
      'http://www.github.com', 'http://www.yahoo.com', 'http://www.etrade.com', 'http://www.cnn.com']
 
     for url in links:
             r = requests.get(url,headers=HEADERS,data=payload)
-            t.sleep(random.randint(1,3))
+            t.sleep(random.randint(sleep1,sleep2))
     count = count+1
-    print("Web Loop Count: ", count)
+
 else:
     time = datetime.now()
     time = time.strftime("%d/%m/%Y %H:%M:%S")
     print("Web Loop End: " + time)
 
-time = datetime.now()
-time = time.strftime("%d/%m/%Y %H:%M:%S")
-
 #Port Scan Loop
 targetlist = ['192.168.1.1', '192.168.1.2']
 
 for target in targetlist:
+    t.sleep(random.randint(sleep1,sleep2))
+    time = datetime.now()
+    time = time.strftime("%d/%m/%Y %H:%M:%S")
     print("Port Scanning: " + target + " at " + time)
-    for i in range(50, 55):
+
+    for i in range(range1, range2):
          s = socket(AF_INET, SOCK_STREAM)
          conn = s.connect_ex((target, i))
-         t.sleep(random.randint(1,3))
+         t.sleep(random.randint(sleep1,sleep2))
          if(conn == 0) :
             print ('Port %d: OPEN ' % (i,))
          s.close()
+
+print("Port Scan Complete")
+print("Finished")
